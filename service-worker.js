2df9a220d0af9c3a028dfce6f88b84f8bd0ceb6b
crossoccerperformancetraining-create <crossoccerperformancetraining@gmail.com>
2026-08-26 05:05:28 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'procoach-v10-athlete-wellness-install';
+const CACHE_NAME = 'procoach-v11-athlete-access-recovery';
 const APP_SHELL = ['./', './index.html', './atleta.html', './manifest.webmanifest', './athlete-manifest.webmanifest', './procoach-icon.svg'];
 self.addEventListener('install', event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
