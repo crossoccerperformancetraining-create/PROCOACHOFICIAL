85084ee84622e7c8c31cc8dc25ea3ff2215775f6
crossoccerperformancetraining-create <crossoccerperformancetraining@gmail.com>
2026-09-04 21:32:00 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'procoach-v61-club-hub';
+const CACHE_NAME = 'procoach-v62-complement-training';
 const APP_SHELL = ['./', './index.html', './atleta.html', './manifest.webmanifest', './athlete-manifest.webmanifest', './procoach-icon.svg', './procoach-fcm-config.js'];
 
 try {
